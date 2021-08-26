9dde9b87dd5a707634103666b1bf43c319c15522
Sergio <87013586+sergiogh7@users.noreply.github.com>
2021-08-26 22:15:38 +0200

    sesion26-t26-spring-rest-mysql

---
+++ main/java/app/Sesion26T26SpringrestmysqlApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sesion26T26SpringrestmysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sesion26T26SpringrestmysqlApplication.class, args);
+	}
+
+}
